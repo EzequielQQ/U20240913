0181245edf78144ebdf8c5f2c13614192b017ad1
Elizabeth <82403771+dinora93@users.noreply.github.com>
2024-08-22 11:26:35 -0600

    Update App2.js

--- App2.js
+++ App2.js
@@ -6,5 +6,12 @@ let rl= readline.createInterface(
 )
 
 rl.question(`Cual es tu nombre?` , (nombre)=>{
-    rl.question(`cual essu apellido` , (apellido)=>{})
+    rl.question(`cual essu apellido` , (apellido)=>{
+        //el ejercicio estaba inconpleto pues unicamente solicita la información pero no la muestra
+        console.log(`Hola, ${nombre} ${apellido}`)
+    //falto el cierre de la interfaz de entrada y salida 
+    rl.close()
+
+    })
+   
 })
